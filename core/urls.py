14cf7e145a8ba756471563a2667eb907332c1cee
poppykode <ngonimug@gmail.com>
2024-02-24 06:55:12 +0200

    created gallery files; views, urls, templates and added lightbox gallery

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,18 @@ from .views.general_pages import (
     contact
 )
 
+from .views.gallery import (
+    folders,
+    folders_images
+
+)
+
 app_name = 'core'
 
 urlpatterns = [
     path('',home, name='home'),
-    path('contact-us',contact, name='contact')
+    path('contact-us',contact, name='contact'),
+    path('our-gallery-folders',folders, name='folders'),
+    path('folders/images/<slug:slug>',folders_images, name='folders_images')
 
 ]
---
+++ core/views/gallery.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
+from core.models import (
+    Folder,
+    Image
+)
+
+def folders(request):
+    template_name = 'gallery/folders.html'
+    folders_ = Folder.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(folders_, 9)
+    folders = paginator.page(page)
+    context = {
+        'folders':folders,
+    }
+    return render(request, template_name, context)
+
+def folders_images(request,slug):
+    template_name = 'gallery/folders_images.html'
+    folder = get_object_or_404(Folder,slug=slug)
+    images_ = Image.objects.filter(folder = folder)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(images_, 12)
+    images = paginator.page(page)
+    context = {
+        'obj':folder,
+        'images':images
+    }
+    return render(request, template_name, context)
